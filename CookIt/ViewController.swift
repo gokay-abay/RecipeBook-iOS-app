0e390ec72ee525761807a1c9bdd0dc12150b108e
Gökay Abay <abaygokay@gmail.com>
2021-01-19 10:11:20 -0800

    initial commit

---
+++ CookIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookIt
+//
+//  Created by Gokay Abay on 2021-01-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
+
